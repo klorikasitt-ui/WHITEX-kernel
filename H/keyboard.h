f64d98bbbcc0f4c1da581fee140f4c997c0eccb6
BURAK YAKUB GÜÇER <klorikasitt@gmail.com>
2026-05-16 14:21:54 +0300

    Add files via upload

--- H/keyboard.h
+++ H/keyboard.h
@@ -1,15 +1,12 @@
-#include <stdint.h>
-#include <stddef.h>
 
 extern void terminal_backspace();
 extern void putchar(char c);
-extern uint8_t inb(uint16_t port);
 
 static int shift_pressed = 0;
 static int caps_lock = 0;
 static int is_extended = 0;
 
-unsigned char kbd_us[128] = {
+static unsigned char kbd_us[128] = {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
     '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
     0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
@@ -17,7 +14,7 @@ unsigned char kbd_us[128] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-unsigned char kbd_us_shift[128] = {
+static unsigned char kbd_us_shift[128] = {
     0,  27, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\b',
     '\t', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',
     0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', 0,
@@ -25,9 +22,9 @@ unsigned char kbd_us_shift[128] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-char input_buffer[256];
-int buffer_idx = 0;
-int input_complete = 0;
+static char input_buffer[256];
+static int buffer_idx = 0;
+static int input_complete = 0;
 
 void keyboard_handler() {
     uint8_t scancode = inb(0x60);
